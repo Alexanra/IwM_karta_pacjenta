a83e3b428adfb2c41fe8235e304c5b392c530f51
Zuzanna Kocur <zuzanna.kocur@gmail.com>
2018-06-12 20:52:35 +0200

    Try to add editing function

---
+++ src/main/java/PatientCard/EditPatientController.java
@@ -0,0 +1,46 @@
+package PatientCard;
+
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import org.hl7.fhir.dstu3.model.Address;
+import org.hl7.fhir.dstu3.model.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class EditPatientController {
+    @Autowired
+    MyFhirClient mfc;
+
+    @GetMapping("/patient={id:.+}/edit")
+    public String editData(@PathVariable("id") String id,
+                           @ModelAttribute("city") String city,
+                           @ModelAttribute("country") String country,
+                           @ModelAttribute("postalCode") String postalCode,
+                           Model model) {
+        Address address = new Address();
+        address.setCity(city);
+        address.setCountry(country);
+        address.setPostalCode(postalCode);
+        ArrayList<Address> addresses = new ArrayList<>();
+        addresses.add(address);
+        Patient patient = new Patient();
+        patient.setAddress(addresses);
+        patient.setId(id);
+
+        MethodOutcome outcome = mfc.client.update()
+                .resource(patient)
+                .execute();
+
+        return "redirect:/patient="+id;
+    }
+}
+
