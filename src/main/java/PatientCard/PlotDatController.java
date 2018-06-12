fd54865418df91a1f7c67079ce948c08aa832b1f
Zuzanna Kocur <zuzanna.kocur@gmail.com>
2018-06-12 18:30:23 +0200

    Get data to create plot

--- src/main/java/PatientCard/PlotDatController.java
+++ src/main/java/PatientCard/PlotDatController.java
@@ -24,39 +24,38 @@ public class PlotDatController {
 
     @GetMapping("/patient={id:.+}/observation={name:.+}")
     public String observationPlotData(@PathVariable("id") String id, @PathVariable("name") String name, Model model) {
-        ArrayList<Observation> observations = new ArrayList<>();
-        //name = "Body Weight";
-        name = "Estimated Glomerular Filtration Rate";
+        ArrayList<String[]> observations = new ArrayList<>();
+        //name = "Estimated Glomerular Filtration Rate";
         Bundle o = this.mfc.client
                 .search()
                 .forResource(Observation.class)
                 .where(new ReferenceClientParam("patient").hasId(id))
                 .returnBundle(Bundle.class)
                 .execute();
-        getObseravtionsFormBundle(o, observations, name);
+        try {
+            getObseravtionsFormBundle(o, observations, name);
+        } catch (FHIRException e) {
+            e.printStackTrace();
+        }
         model.addAttribute("observations", observations);
         return "patientPlot";
     }
 
-    private void getObseravtionsFormBundle (Bundle bundle, ArrayList<Observation> list, String name) {
+    private void getObseravtionsFormBundle (Bundle bundle, ArrayList<String[]> list, String name) throws FHIRException {
         for(ListIterator<Bundle.BundleEntryComponent> iter = bundle.getEntry().listIterator(); iter.hasNext(); ) {
             Observation observation = (Observation) iter.next().getResource();
             if(observation.getCode().hasText()){
-                if (observation.getCode().getText().equals(name)) {
-                    list.add(observation);
-                    try {
-                        observation.getValueQuantity().getValue();
-                        observation.getValueQuantity().getCode();
+                if (observation.getCode().getText().replace(" ", "_").equals(name)) {
+                    String[] array = new String[4];
+                    array[0] = observation.getCode().getText();
+                    array[1] = observation.getEffectiveDateTimeType().toHumanDisplay();
+                    array[2] = String.valueOf(observation.getValueQuantity().getValue());
+                    array[3] = observation.getValueQuantity().getCode();
 
-                    } catch (FHIRException e) {
-                        e.printStackTrace();
-                    }
-
+                    list.add(array);
                 }
                 //observation.getEffectiveDateTimeType().toHumanDisplay()
                 ;
-            } else if (observation.hasComponent()) {
-                observation.getComponent();
             }
 
 
